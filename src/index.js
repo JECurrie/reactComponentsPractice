a7e39085dc2259739b2f843bcab87ff35b2cfb22
Joan Currie <jecurrie8@gmail.com>
2021-01-15 08:33:30 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  document.getElementById("root")
+);
